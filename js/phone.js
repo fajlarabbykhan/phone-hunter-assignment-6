c5cec102a6d8aaf91a6956d322d07a0d9d763c04
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-03-01 09:39:00 +0600

    Search field and buttone done

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,12 @@
-console.log("hello")
+// console.log("hello")
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value
+    // console.log(searchText)
+
+    searchField.value = ""
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+}
